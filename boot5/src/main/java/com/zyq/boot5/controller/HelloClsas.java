c20ddeeccb6554978401ff2b217e255ab3c3f797
Zhengyunqing <1365460054@qq.com>
2020-11-08 19:49:19 +0800

    测试提交第一次项目

---
+++ boot5/src/main/java/com/zyq/boot5/controller/HelloClsas.java
@@ -0,0 +1,7 @@
+package com.zyq.boot5.controller;
+
+public class HelloClsas {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
---
+++ boot5/src/test/java/com/zyq/boot5/Boot5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyq.boot5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
