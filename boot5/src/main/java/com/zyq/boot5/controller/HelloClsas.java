27e3dc0b0616e174d20255fa871a3f555ae775ba
Zhengyunqing <1365460054@qq.com>
2020-11-08 21:34:44 +0800

    开发人员提交的

--- boot5/src/main/java/com/zyq/boot5/controller/HelloClsas.java
+++ boot5/src/main/java/com/zyq/boot5/controller/HelloClsas.java
@@ -2,6 +2,10 @@ package com.zyq.boot5.controller;
 
 public class HelloClsas {
     public static void main(String[] args) {
+
         System.out.println("你好");
+
+        //开发人员写了一句话
+        System.out.println("我更新了");
     }
 }
